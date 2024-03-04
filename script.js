a266f13bf81115ac0bee940c86d5239d00ab6c20
rithielly-becker <159202697+rithielly-becker@users.noreply.github.com>
2024-03-04 19:37:11 -0300

    dia 18

--- script.js
+++ script.js
@@ -40,23 +40,35 @@ function calcular() {
 
 const localStorageKey = 'to-do-list-gn'
 
+function validateIfExistsNewTask() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let inputValue = document.getElementById('input-new-task').value
+    let exists = values.find(x => x.name == inputValue)
+    return !exists ? false : true
+}
+
 function newTask() {
 
     let input = document.getElementById('input-new-task')
-
+    input.style.border = ''
     //validação
 
     if (!input.value) {
+        input.style.border = '1px solid red'
         alert('Digite algo para incerir em sua lista')
     }
+    else if (validateIfExistsNewTask()) {
+        alert('ja existe uma task com essa descrição')
+    }
     else {
-        let values = JSON.parse(localStorage.getItem('') || "[]")
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
         values.push({
             name: input.value
         })
         localStorage.setItem(localStorageKey, JSON.stringify(values))
         showValues()
     }
+    input.value = ''
 }
 
 function showValues() {
@@ -65,6 +77,16 @@ function showValues() {
     let list = document.getElementById('to-do-list')
     list.innerHTML = ''
     for (let i = 0; i < values.length; i++) {
-        list.innerHTML += `<li>${values[i]['name']}</li>`;
+        list.innerHTML += `<li>${values[i]['name']}<button id='btn-ok' onclick='removeItem("${values[i]['name']}")'>ok</button></li>`;
     }
 }
+
+function removeItem(data) {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let index = values.findIndex(x => x.name == data)
+    values.splice(index, 1)
+    localStorage.setItem(localStorageKey, JSON.stringify(values))
+    showValues()
+}
+
+showValues()
